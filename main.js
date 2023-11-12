d749d35ad540cc106be8b028f62e55cbe2c58542
AKi <aaronisberg@protonmail.com>
2023-11-11 19:14:30 -0800

    Base Babylon Scaffold using VITE

---
+++ main.js
@@ -0,0 +1,20 @@
+import * as BABYLON from '@babylonjs/core';
+
+const canvas = document.getElementById('renderCanvas');
+
+const engine = new BABYLON.Engine(canvas);
+
+const createScene = function() {
+  const scene = new BABYLON.Scene(engine);
+
+  scene.createDefaultCameraOrLight(true, false, true);
+  const box = new BABYLON.MeshBuilder.CreateBox();
+
+  return scene;
+}
+
+const scene = createScene();
+
+engine.runRenderLoop(function() {
+  scene.render();
+});
